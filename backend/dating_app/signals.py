2681c1aaf5754db0f40efccb6778119b09f2e9ee
jess <148654722+jessxox@users.noreply.github.com>
2025-02-19 16:39:46 -0500

    Update signals.py

--- backend/dating_app/signals.py
+++ backend/dating_app/signals.py
@@ -1,9 +1,9 @@
-nano signals.py 
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile  # Import Profile model
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
