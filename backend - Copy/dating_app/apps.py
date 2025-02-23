1d293d8182d5d3a2cc02e5509181e5e44b44cb0d
igor74113 <ipinchuk@dons.usfca.edu>
2025-02-23 07:18:23 -0800

    Commit all changes before pushing

--- backend - Copy/dating_app/apps.py
+++
@@ -1,7 +0,0 @@
-# this folder contains django apps configurations 
-from django.apps import AppConfig
-
-
-class DatingAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "dating_app"
